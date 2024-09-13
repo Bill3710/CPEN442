9e7f3350d0755995fcdb17b583c4f571f0b36859
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-09-13 14:25:57 -0700

    Add initial

---
+++ A1/a1.py
@@ -0,0 +1,28 @@
+
+
+
+def index_of_coincidence(text: str) -> float:
+    """Returns the index of coincidence of a text, assuming the alphabet is of size 26"""
+    # Write your function here
+    return 1.
+
+
+def vigenere_encrypt(plaintext: str, key: str) -> str:
+    """Encrypts a plaintext with the given key using the Vigenere cipher, returning the ciphertext. You may assume the plaintext and key are strings that only contain ascii uppercase characters."""
+    return ''
+
+def vigenere_decrypt(ciphertext: str, key: str) -> str:
+    """Decrypts a ciphertext with the given key using the Vigenere cipher, returning the plaintext. You may assume the ciphertext and key are strings that only contain ascii uppercase characters."""
+    return ''
+
+
+def crack_key_length_vigenere(ciphertext: str) -> int:
+    """Returns the length of the key that was used to generate the given ciphertext with a Vigenere cipher"""
+    return 0
+
+
+def crack_vigenere(ciphertext: str) -> tuple[str, str]:
+    """Given a ciphertext generated with the Vigenere cipher, this function cracks the secret key and returns both the key and the plaintext."""
+    key = ''
+    plaintext = ''
+    return key, plaintext
