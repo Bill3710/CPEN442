a943fea2364459f062649ef4b084280de7e5344c
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-09-16 21:33:28 -0700

    Update a1.py

--- A1/a1.py
+++ A1/a1.py
@@ -30,54 +30,91 @@ def index_of_coincidence(text: str) -> float:
 def vigenere_encrypt(plaintext: str, key: str) -> str:
     """Encrypts a plaintext with the given key using the Vigenere cipher, returning the ciphertext. You may assume the plaintext and key are strings that only contain ascii uppercase characters."""
     if plaintext == "" or key == "":
-        return -1
+        return ""
+    
     offsetASCII = 65 # ASCII for 'A'
     ciphertext = ''
-    for charK in key:
-        for charP in plaintext:
-            ciphertext += chr(ord(charK) + ord(charP) - offsetASCII)
+    key_length = len(key)
+
+    for i, char in enumerate(plaintext):
+        key_char = key[i % key_length]
+        encrypted_char = (ord(char) + ord(key_char) - 2 * offsetASCII) % 26 + offsetASCII
+        ciphertext += chr(encrypted_char)
+
     return ciphertext
 
 def vigenere_decrypt(ciphertext: str, key: str) -> str:
     """Decrypts a ciphertext with the given key using the Vigenere cipher, returning the plaintext. You may assume the ciphertext and key are strings that only contain ascii uppercase characters."""
     if ciphertext == "" or key == "":
-        return -1
+        return ""
+    
     offsetASCII = 65 # ASCII for 'A'
     plaintext = ''
-    for charK in key:
-        for charC in ciphertext:
-            if ord(charC) < ord(charK):
-                plaintext += chr(ord(charC) + 26 - ord(charK) + offsetASCII)
-            else :
-                plaintext += chr(ord(charC) - ord(charK) + offsetASCII)
+    key_length = len(key)
+
+    for i, char in enumerate(ciphertext):
+        key_char = key[i % key_length] 
+        decrypted_char = (ord(char) - ord(key_char) + 26) % 26 + offsetASCII
+        plaintext += chr(decrypted_char)
+
     return plaintext
 
 
 def crack_key_length_vigenere(ciphertext: str) -> int:
     """Returns the length of the key that was used to generate the given ciphertext with a Vigenere cipher"""
-    englishIC = 1.73
-    minIC_Diff = 100 # just a really high number for default
-    minIC_DiffKeylength = 0
-    for keylength in range (1, len(ciphertext)+1):
-        curTotalIC = 0
-        for i in range (0, keylength):
-            curCharGroup = ciphertext[i::keylength]
-            curTotalIC += index_of_coincidence(curCharGroup)
-        averageIC = curTotalIC / keylength
-        
-        if minIC_Diff > abs(englishIC - averageIC):
-            minIC_Diff = abs(englishIC - averageIC)
-            minIC_DiffKeylength = keylength
-            
-    return minIC_DiffKeylength
+    keyLengths = range(2, 21)
+    best_length = 0
+    best_ic = 0.0
 
+    for key_length in keyLengths:
+            subsequences = []
+            for _ in range(key_length):
+                subsequences.append('')
+
+            for i, char in enumerate(ciphertext):
+                subsequences[i % key_length] += char
+
+            total_ic = 0.0
+            for sub in subsequences:
+                total_ic += index_of_coincidence(sub)
+            average_ic = total_ic / key_length
+
+            if average_ic > best_ic:
+                best_ic = average_ic
+                best_length = key_length
+
+    return best_length
 
 def crack_vigenere(ciphertext: str) -> tuple[str, str]:
     """Given a ciphertext generated with the Vigenere cipher, this function cracks the secret key and returns both the key and the plaintext."""
     key = ''
     plaintext = ''
-    return key, plaintext
 
+    key_length = crack_key_length_vigenere(ciphertext)
+    subsequences = []
+
+    for _ in range(key_length):
+        subsequences.append('')
+
+    for i, char in enumerate(ciphertext):
+        subsequences[i % key_length] += char
+
+    for i, sub in enumerate(subsequences):
+        freqs = {}
+        for char in sub:
+            if char in freqs:
+                freqs[char] += 1
+            else:
+                freqs[char] = 1
+
+        most_freq_char = max(freqs, key=freqs.get)
+        shift = (ord(most_freq_char) - ord('E')) % 26
+        key += chr((ord('A') + shift) % 26 + ord('A'))
+
+    plaintext = vigenere_decrypt(ciphertext, key)
+
+    return key, plaintext
+    
 # vigenere_encrypt test #
 
 plain1 = open("plaintext1.txt", "r").read()
