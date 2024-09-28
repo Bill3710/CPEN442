64e6488fdf02b3bf3ac10afa24fdc0cf7261eda9
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-09-27 21:52:14 -0700

    2.2， 2.3 done

--- A2/a2.py
+++ A2/a2.py
@@ -5,6 +5,7 @@ You can implement other functions and import other packages"""
 from typing import Callable
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Util import Counter
 
 
 def xor_bytes(input1: bytes, input2: bytes) -> bytes:
@@ -27,23 +28,33 @@ def decrypt_aes_ecb(ciphertext: bytes, key: bytes) -> bytes:
     return bytes(1)
 
 def encrypt_aes_cbc(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
-    """Encrypts the plaintext with the given key and IV using AES in CBC mode. Returns the cyphertext as a bytes object"""
-    return bytes(1)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_text = pad(plaintext, AES.block_size)
+    ciphertext = cipher.encrypt(padded_text)
+    return ciphertext
 
 def decrypt_aes_cbc(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
     """Decrypts the ciphertext with the given key and iv using AES in CBC mode. Returns the plaintext as a bytes object"""
-    return bytes(1)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plaintext = cipher.decrypt(ciphertext)
+    plaintext = unpad(padded_plaintext, AES.block_size)
+    return plaintext
 
 
 def encrypt_aes_ctr(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
     """Encrypts the plaintext with the given key and IV using AES in CTR mode. Returns the cyphertext as a bytes object"""
-    return bytes(1)
+    ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+    cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
+    ciphertext = cipher.encrypt(plaintext)
+    return ciphertext
 
 
 def decrypt_aes_ctr(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
     """Decrypts the ciphertext with the given key and IV using AES in CTR mode. Returns the plaintext as a bytes object"""
-    return bytes(1)
-
+    ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder='big'))
+    cipher = AES.new(key, AES.MODE_CTR, counter=ctr)
+    plaintext = cipher.decrypt(ciphertext)
+    return plaintext
 
 class AttackCTR:
 
