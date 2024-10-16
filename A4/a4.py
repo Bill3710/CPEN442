482caa2a3c0cf7d8f8a788861f51fb0509b00736
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-10-15 17:20:54 -0700

    Update a4.py

--- A4/a4.py
+++ A4/a4.py
@@ -1,5 +1,6 @@
 from typing import Callable
 import time
+import numpy as np
 
 def online_attack(check_password: Callable) -> str:
     # TODO Implement attack here by calling check_password
@@ -8,24 +9,36 @@ def online_attack(check_password: Callable) -> str:
     
     guessed_password = ""
 
-    while True:
+    maxlength = 10
+
+    while len(guessed_password) <= maxlength:
         maxTime = -1
-        current_char = None
-        for char in charset:
-            gues = guessed_password + char
-            start = time.time()
-            check_password(gues)
-            timeGues = time.time() - start
-            print("guess", timeGues)
+        # current_char = None
+        times = np.zeros(len(charset))
 
-            if timeGues > maxTime:
-                maxTime = timeGues
-                current_char = char
+        for i, char in enumerate(charset):
+            
+            temp = guessed_password + char
+            start = time.perf_counter()
+            check_password(temp)
+            times[i] = time.perf_counter() - start
+            # print(times[i] > (maxTime + 0.0001), maxTime, char, "result \n")
 
-        if current_char is None:
-            raise ValueError("No valid character found; check timing logic.")
+            # if times[i] >= maxTime + 0.0001:
+            #     maxTime = times[i]
+            #     current_char = char
+        index = 0
+        for i in range(0,len(times)):
+            if times[i] >= maxTime:
+                maxTime = times[i]
+                index = i
+
+        guessed_password += charset[index]
         
-        guessed_password += current_char
-
+        # guessed_password += current_char
+        # print("more char \n", guessed_password)
         if check_password(guessed_password):
+            print("password find!!!\n")
             return guessed_password
+        
+    return guessed_password
