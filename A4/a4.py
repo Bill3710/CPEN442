9f819b33cabeed4fb48297bb027728d77e8f73ba
Bill3710 <112989619+Bill3710@users.noreply.github.com>
2024-10-17 18:29:53 -0700

    should pass

--- A4/a4.py
+++ A4/a4.py
@@ -4,32 +4,44 @@ import numpy as np
 
 def online_attack(check_password: Callable) -> str:
     charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
     length = find_length(check_password)
-    
     guessed_password = ['A'] * length
-    
-    for i in range(length):
-        maxTime = -0.001
+    maxTime = np.zeros(length)
+    i = 0
+
+    while i < length:
         times = np.zeros(len(charset))
 
-        for j in range(len(charset)):
-            guessed_password[i] = charset[j]
-            start = time.perf_counter()
-            check_password(''.join(guessed_password))
-            times[j] = time.perf_counter() - start
+        if i == length - 1:
+            for char in charset:
+                guessed_password[i] = char
+                curr_pwd = ''.join(guessed_password)
+                if check_password(curr_pwd):
+                    return curr_pwd
+        else:
+ 
+            for j in range(len(charset)):
+                guessed_password[i] = charset[j]
+                curr_pwd = ''.join(guessed_password)
+                
+                start = time.perf_counter()
+                check_password(curr_pwd)
+                times[j] = time.perf_counter() - start
+            
+            maxIndex = np.argmax(times)
+            current_max_time = times[maxIndex]
 
 
-        for k in range(len(times)):
-            if times[k] > maxTime:
-                maxTime = times[k]
-                guessed_password[i] = charset[k]
-        
-        if check_password(guessed_password):
-            print("password find!!!\n")
-            return guessed_password
-    
+            if current_max_time - maxTime[i - 1] >= 0.0003:  
+                guessed_password[i] = charset[maxIndex]
+                maxTime[i] = current_max_time
+                i += 1
+            else:
+                print("Revert, current index is", i, "Max Time Difference:", current_max_time - maxTime, "current passward:", ''.join(guessed_password))
+                i = max(0, i - 1)
+
     return ''.join(guessed_password)
+
 
 def find_length(check_password: Callable) -> int:
 
